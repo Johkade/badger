3ccb392b839180ee5dca5496f19863d2f66374e8
Johannes <j.kadeii@gmx.de>
2020-02-19 13:36:00 +0100

    test

--- crawl.bash
+++ crawl.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-timestamp = $(geth --exec 'eth.getBlock(3150).timestamp' attach)
+timestamp=$(geth --exec 'eth.getBlock(3150).timestamp' attach)
 echo timestamp: $timestamp
 
-blockhash = $(geth --exec 'eth.getBlock(3150).hash' attach)
+blockhash=$(geth --exec 'eth.getBlock(3150).hash' attach)
 echo blockhash: $blockhash
 
 
-txs = $(geth --exec 'eth.getBlock(3150).transactions' attach)
-tx_num = ${#txs[@]}
+txs=$(geth --exec 'eth.getBlock(3150).transactions' attach)
+tx_num=${#txs[@]}
 
 echo "number of transactions in block 3150:" $tx_num
 
@@ -17,6 +17,6 @@ echo "number of transactions in block 3150:" $tx_num
 
 for i in "${txs[@]}"
 do
-	txhash = $i.hash
+	txhash=$i.hash
 	echo $(geth --exec 'eth.getTransaction("$txhash").to' attach)
 done
