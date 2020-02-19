8c24b0186c6ef676e233b5a7a4539533d6bf7656
Johannes <j.kadeii@gmx.de>
2020-02-19 13:09:29 +0100

    test

--- crawl.bash
+++ crawl.bash
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 
-geth --exec 'eth.getBlock("latest").nonce' attach
+timestamp = geth --exec 'eth.getBlock(3150).timestamp' attach
+echo timestamp: $timestamp
+
+blockhash = geth --exec 'eth.getBlock(3150).hash' attach
+echo blockhash: $blockhash
+
+
+txs = geth --exec 'eth.getBlock(3150).transactions' attach
+tx_num = ${#txs[@]}
+
+echo number of transactions in block 3150: $tx_num
+
+
+
+for i in "${txs[@]}"
+do
+	txhash = $i.hash
+	echo geth --exec 'eth.getTransaction("$txhash").to' attach
+done
