339df25d8b030a4d3ee53f4d0ee99b2f1785c47b
Johannes <48554608+Johkade@users.noreply.github.com>
2020-02-23 15:28:01 -0800

    Update honeypot_test.bash
    some very minor final changes concerning writing the results to file.

--- honeypot_test.bash
+++ honeypot_test.bash
@@ -29,8 +29,29 @@ do
 		touch "/to_check/${addr__}.evm"
 		echo $bytecode__ > "/to_check/${addr__}.evm" 
 
+		#check whether and which type of honeypot by checking the commands output ((not very nice code :I)
+		#and write  honeypot address and technique to file honeypots.csv where changes will be monitored and published to twitter
 		result=$(eval $honeypot_check)
-		echo result is: $result
+		is_honeypot=0
+		if [[ $result == *"Hidden transfer:        True"* ]];then
+                	printf "$addr, Hidden transfer" >> ./honeypots.csv
+		elif [[ $result == *"Balance disorder: 	 True"* ]];then
+			printf "$addr, Balance Disorder" >> ./honeypots.csv
+		elif [[ $result == *"Inheritance disorder:   True"* ]];then
+                        printf "$addr, Inheritance disorder" >> ./honeypots.csv
+		elif [[ $result == *"Uninitialised struct:   False"* ]];then
+                        printf "$addr, Uninitialised struct" >> ./honeypots.csv
+		elif [[ $result == *"Type overflow:          True"* ]];then
+                        printf "$addr, Type overflow" >> ./honeypots.csv
+		elif [[ $result == *"Skip empty string:      True"* ]];then
+                        printf "$addr, Skip empty string" >> ./honeypots.csv
+		elif [[ $result == *"Hidden state update:    True"* ]];then
+                        printf "$addr, Hidden state update" >> ./honeypots.csv
+		elif [[ $result == *"Straw man contract:     True"* ]];then
+                        printf "$addr, Straw man contract" >> ./honeypots.csv
+		fi
+
+
 		time=$actual
 	fi
 	sleep 5
